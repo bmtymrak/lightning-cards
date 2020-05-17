7a9d8ed7f015106209c1df0e442b86ce4eacd28e
Brennan <bmtymrak@gmail.com>
2020-05-17 17:45:58 -0400

    pre-deply

--- lightning_cards/settings.py
+++ lightning_cards/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY=env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 # DEBUG=False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['lightning-cards.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
