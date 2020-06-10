9c08f1e4be8b4916938178ccb5ed6798a4c49d30
Brennan <bmtymrak@gmail.com>
2020-06-10 12:06:44 -0400

    dev database

--- lightning_cards/settings.py
+++ lightning_cards/settings.py
@@ -102,11 +102,11 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DATABASE_NAME"),
-        'USER': env("DATABASE_USER"),
-        'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': env("DATABASE_HOST"),
-        'PORT': env("DATABASE_PORT"),
+        'NAME': 'lightningcards',
+        'USER': 'brennan',
+        'PASSWORD': 'devpass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
